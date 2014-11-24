343f2d282bc57a2d90f0b80eb04ef4d63c65ed63
auk <i.kvjatkovskij@internetdevels.ua>
2014-11-24 15:02:24 +0200

    Task #209: Added batch to import taxonomy city, added vocabulary the list of cities, changed: autocomplete, field behavior field_events_paid, added formatter for field geolocation. Added modules: geophp, geolocation.

---
+++ modules/custom/idevels/src/Plugin/Field/FieldFormatter/IdevelsLatlngFormatter.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\idevels\Plugin\Field\FieldFormatter\IdevelsLatlngFormatter.
+ */
+
+namespace Drupal\idevels\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+
+/**
+ * Plugin implementation of the 'idevels_latlng' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "idevels_latlng",
+ *   module = "idevels",
+ *   label = @Translation("Idevels map"),
+ *   field_types = {
+ *     "geolocation"
+ *   }
+ * )
+ */
+class IdevelsLatlngFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items) {
+    $element = array();
+
+    foreach ($items as $delta => $item) {
+      $element[$delta] = array(
+        '#theme' => 'idevels_formatter',
+        '#lat' => $item->lat,
+        '#lng' => $item->lng,
+      );
+    }
+    return $element;
+  }
+}
