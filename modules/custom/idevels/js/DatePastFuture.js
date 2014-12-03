3dce88a10012962a988bf31ce7d9c918adc50394
Voron <m.keda@internetdevels.ua>
2014-12-03 16:28:30 +0200

    Task #215: Improve js validation for past date. Still can't add validation for create user page.

--- modules/custom/idevels/js/DatePastFuture.js
+++ modules/custom/idevels/js/DatePastFuture.js
@@ -3,11 +3,67 @@
   // get curent date
   var d = new Date();
 
+  function daysInMonth(month, year) {
+    return new Date(year, month, 0).getDate();
+  }
+
+  function validateDays(month, year, $datediv) {
+    $daySelect = $datediv.find("select[title=Day]");
+    $daySelectOptions = $datediv.find("select[title=Day] option");
+    $dayDefaultOption = $datediv.find("select[title=Day] option:first");
+    // If future Day was selected - select first option
+    if (month == d.getMonth()+1 && year == d.getFullYear()) {
+      if ($daySelect.val() > d.getDate()) {
+        $daySelect.val($dayDefaultOption.val());
+      };
+      $daySelectOptions.each(function(){
+        if (parseInt($(this).val()) > d.getDate()) {
+          $(this).hide();
+        };
+      });
+    }
+    // If user select not future Month - show all Days
+    else {
+      $daySelectOptions.each(function(){
+        console.log(parseInt($(this).val()));
+        if (parseInt($(this).val()) <= daysInMonth(month, year)) {
+          $(this).show();
+        }
+        else {
+          $(this).hide();
+        }
+      });
+    };
+  }
+
+  function validateMonth(month, year, $datediv) {
+    $monthSelect = $datediv.find("select[title=Month]");
+    $monthSelectOptions = $datediv.find("select[title=Month] option");
+    $monthDefaultOption = $datediv.find("select[title=Month] option:first");
+    if (year == d.getFullYear()) {
+      // If future Month was selected - select first option
+      if ($monthSelect.val() > d.getMonth()+1) {
+        $monthSelect.val(monthDefaultOption.val());
+      };
+      $monthSelectOptions.each(function(){
+        if (parseInt($(this).val()) > d.getMonth()+1) {
+          $(this).hide();
+        }
+        else {
+          $(this).show();
+        }
+      });
+    }
+    else {
+      $monthSelectOptions.each(function(){
+        $(this).show();
+      });
+    }
+    validateDays(month, year, $datediv)
+  }
+
   // Validation for html5 input type=date
   $('.past-date input[type=date]').attr({ "max": d.getFullYear().toString() + "-"+ d.getMonth().toString() +"-"+ d.getDate().toString()});
-
-
-  // Validation for date selectbox
 
   // Remove futures years
   $(".past-date select[title=Year] option").each(function(){
@@ -16,45 +72,18 @@
     };
   });
 
-  // If user select curent Year - hide all futures Months
-  $(".past-date select[title=Year]").change(function(event) {
-    if (parseInt($(this).val()) == d.getFullYear()) {
-      $(this).parent().parent().find("select[title=Month] option").each(function(){
-        if (parseInt($(this).val()) > d.getMonth()+1) {
-          $(this).hide();
-        };
-      });
-      // If future Month was selected - select first option
-      if ($(this).parent().parent().find("select[title=Month]").val() > d.getMonth()+1) {
-        $(this).parent().parent().find("select[title=Month]").val($(this).parent().parent().find("select[title=Month] option:first").val());
-      };
-      // If user select curent Month - hide all futures Days
-      $(this).parent().parent().find("select[title=Month]").change(function(event) {
-        if (parseInt($(this).val()) == d.getMonth()+1) {
-          $(this).parent().parent().find("select[title=Day] option").each(function(){
-            if (parseInt($(this).val()) > d.getDate()) {
-              $(this).hide();
-            };
-          });
-          // If future Day was selected - select first option
-          if ($(this).parent().parent().find("select[title=Day]").val() > d.getDate()) {
-            $(this).parent().parent().find("select[title=Day]").val($(this).parent().parent().find("select[title=Day] option:first").val());
-          };
-        }
-        // If user select not curent Month - show all Days
-        else {
-          $(this).parent().parent().find("select[title=Day] option").each(function(){
-            $(this).show();
-          });
-        };
-      });
-    }
-    // If user select not curent Year - show all Months
-    else {
-      $(this).parent().parent().find("select[title=Month] option").each(function(){
-        $(this).show();
-      });
-    };
+  $(".past-date").each(function(){
+    validateMonth(parseInt($(this).find("select[title=Month]").val()), parseInt($(this).find("select[title=Year]").val()), $(this));
+  });
+
+  // Validation for date selectbox on Month chenged
+  $(".past-date").on('change', 'select[title=Month]', function(event) {  
+    validateDays(parseInt($(this).val()), parseInt($(this).parent().parent().find("select[title=Year]").val()), $(this).parent().parent());
+  });
+
+  // Validation for date and Month selectbox on Year chenged
+  $(".past-date").on('change', 'select[title=Year]', function(event) {  
+    validateMonth(parseInt($(this).parent().parent().find("select[title=Month]").val()), parseInt($(this).val()), $(this).parent().parent());
   });
 
 })})(jQuery);
