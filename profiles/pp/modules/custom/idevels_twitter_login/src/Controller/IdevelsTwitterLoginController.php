4cd3371b58cfb55d9728eb9698c1e34d5839a1ba
Vadym Abramchuk <abramm@gmail.com>
2015-03-31 21:04:46 +0300

    Partial twitter module, not ready yet. Commiting to save progress

---
+++ profiles/pp/modules/custom/idevels_twitter_login/src/Controller/IdevelsTwitterLoginController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Drupal\idevels_twitter_login\Controller;
+
+use Abraham\TwitterOAuth\TwitterOAuth;
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+
+
+class IdevelsTwitterLoginController extends ControllerBase {
+
+  public function unifiedLoginRegister() {
+    $idevels_twitter_login_config = $this->config('idevels_twitter.system');
+    $consumer_key = $idevels_twitter_login_config->get('idevels_twitter_consumer_key');
+    $secret_key = $idevels_twitter_login_config->get('idevels_twitter_consumer_secret');
+    if (!$consumer_key || strlen($consumer_key) == 0)
+      throw new \Exception('Missing Twitter consumer key.');
+    if (!$secret_key|| strlen($consumer_key) == 0)
+      throw new \Exception('Missing Twitter secret key.');
+    libraries_load('twitteroauth');
+
+    $session = new Session();
+
+    if (isset($_GET['oauth_token'])) {
+      $request_token['oauth_token'] = $session->get('oauth_token');
+      $request_token['oauth_token_secret'] = $session->get('oauth_token_secret');
+
+      // This is very, very horrible
+      if ($request_token['oauth_token'] !== $_GET['oauth_token']) {
+        drupal_set_message(t('oauth_token mismatch'), 'error');
+        return (new RedirectResponse(\Drupal::url('<front>')));
+      }
+      $connection = new TwitterOAuth($consumer_key, $secret_key, $request_token['oauth_token'], $request_token['oauth_token_secret']);
+      $access_token = $connection->oauth("oauth/access_token", array("oauth_verifier" => $_GET['oauth_verifier']));
+
+      // TODO: Register or log in user
+
+    } else {
+      $connection = new TwitterOAuth($consumer_key, $secret_key);
+      $request_token = $connection->oauth(
+        'oauth/request_token',
+        array('oauth_callback' => \Drupal::url('idevels_twitter_login', array(), array('absolute' => true)))
+      );
+      $session->set('oauth_token', $request_token['oauth_token']);
+      $session->set('oauth_token_secret', $request_token['oauth_token_secret']);
+      $session->save();
+
+      return new RedirectResponse(
+        $connection->url(
+          'oauth/authorize',
+          array('oauth_token' => $request_token['oauth_token'])
+        )
+      );
+    }
+  }
+
+  /**
+   * Load user by Twitter ID
+   * @param $id int Twitter User ID
+   * @return \Drupal\user\Entity\User Found user or null if none exists
+   */
+  public static function loadUserByTwitterId($id)
+  {
+    $user_ids = \Drupal::entityQuery('user')
+      ->condition('field_twitter_user_id', $id)
+      ->execute();
+    if (!$user_ids)
+      return null;
+    return entity_load('user', reset($user_ids));
+  }
+
+  /**
+   * Load user by Twitter ID.
+   * This method also registers new user if none found.
+   * @param $id int Twitter User ID
+   * @param $connection TwitterOAuth API Connection object
+   * @return \Drupal\user\Entity\User Found or newly created user
+   */
+  public static function loadOrRegisterUserByTwitterId($id, TwitterOAuth $connection)
+  {
+    $foundUser = self::loadUserByTwitterId($id);
+    if ($foundUser)
+      return $foundUser;
+
+    $userInfo = $connection->get("account/verify_credentials");
+    // TODO: Register user if none found
+
+  }
+
+}
---
+++ profiles/pp/modules/custom/idevels_twitter_login/src/Form/IdevelsLoginSettingsForm.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Drupal\idevels_twitter_login\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class IdevelsLoginSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'system_idevels_twitter_login_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $idevels_twitter_login_config = $this->config('idevels_twitter.system');
+    $form['idevels_twitter_consumer_key'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Consumer Key (API Key)'),
+      '#required' => TRUE,
+      '#description' => t('Consumer Key (API Key) of Twitter application.'),
+      '#default_value' => $idevels_twitter_login_config->get('idevels_twitter_consumer_key'),
+    );
+    $form['idevels_twitter_consumer_secret'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Consumer Secret (API Secret)'),
+      '#required' => TRUE,
+      '#description' => t('Consumer Secret (API Secret) of Twitter application.'),
+      '#default_value' => $idevels_twitter_login_config->get('idevels_twitter_consumer_secret'),
+    );
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $this->configFactory()->getEditable('idevels_twitter.system')
+      ->set('idevels_twitter_consumer_key', $form_state->getValue('idevels_twitter_consumer_key'))
+      ->set('idevels_twitter_consumer_secret', $form_state->getValue('idevels_twitter_consumer_secret'))
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+  
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['idevels_twitter.system'];
+  }
+}
